6a820dc8e4fe3904eaecf0c60296f6f682a40a11
eloback <eduardolloback@gmail.com>
2021-10-18 11:14:31 -0300

    comments

--- segmentation.py
+++ segmentation.py
@@ -4,20 +4,20 @@ import os, random
 
 path = "./dataset/"
 
-def segmentDiceUsingCanny(image):
+def getCountoursUsingCanny(image):
     rect, thresh = cv.threshold(image, 125, 255, cv.THRESH_BINARY, image)
     gray = cv.cvtColor(thresh, cv.COLOR_BGR2GRAY)
     edges = cv.Canny(gray, 50, 150, apertureSize=3)
-    return edges
+    contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    return edges, contours
 
-def segmentDice(image):
+def getCountours(image):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     rect, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
-    return thresh
+    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    return thresh, contours
 
-def extractDices(segmentedImage, image):
-    copy = image.copy()
-    contours, hierarchy = cv.findContours(segmentedImage, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+def segmentAndDrawDices(contours, image):
     rects = []
     for cnt in contours:
         rect = cv.minAreaRect(cnt)
@@ -27,23 +27,24 @@ def extractDices(segmentedImage, image):
         rects.append(rect)
         box = cv.boxPoints(rect)
         box = np.int0(box)
-        cv.drawContours(copy, [box], 0, (0, 0, 255), 2)
-    return copy, rects
+        cv.drawContours(image, [box], 0, (0, 0, 255), 2)
+    return image, rects
 
 for i in range(0, 2):
     file = random.choice(os.listdir(path))
     img = cv.imread(path + file)
-    segmentedImage = segmentDice(img)
-    result, rects = extractDices(segmentedImage, img)
-    print("number of dices in "+str(i) + " is " + str(len(rects)))
-    #print countours on a blank image
-    cv.imshow("contours"+str(i), result)
-    cv.moveWindow("contours"+str(i), img.shape[1]*2+1, img.shape[0]*i+1)
+    result, contours = getCountours(img)
+    segmentedImage, rects = segmentAndDrawDices(contours, img.copy())
+    
     #Show the Original image
-    cv.imshow("Original"+str(i), img)
-    cv.moveWindow("Original"+str(i), 1, img.shape[0]*i+1)
+    cv.imshow("Original "+file, img)
+    cv.moveWindow("Original "+file, 1, img.shape[0]*i+1)
+    #show the product of the contours extraction
+    cv.imshow(file, result)
+    cv.moveWindow(file, img.shape[1], img.shape[0]*i+1)
     #Show the segmented dice
-    cv.imshow(str(i), segmentedImage)
-    cv.moveWindow(str(i), img.shape[1], img.shape[0]*i+1)
+    cv.imshow("contours "+file, segmentedImage)
+    cv.moveWindow("contours "+file, img.shape[1]*2+1, img.shape[0]*i+1)
+    print("number of dices in "+file + " is " + str(len(rects)))
     
 cv.waitKey(0)
