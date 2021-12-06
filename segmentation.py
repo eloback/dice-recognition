af262fc61825a51a4d691d59488b0f60a7c68674
Eloback <eduardolloback@gmail.com>
2021-12-06 15:15:05 -0300

    dice count

--- segmentation.py
+++ segmentation.py
@@ -16,7 +16,7 @@ def getCountours(image):
     #cv.bitwise_not(thresh, thresh)
     #thresh = cv.erode(thresh, (5, 5) iterations=6)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    return thresh, contours
+    return contours, hierarchy
 
 #alternative way of getting countours
 def getCountoursUsingCanny(image):
@@ -24,37 +24,82 @@ def getCountoursUsingCanny(image):
     gray = cv.cvtColor(thresh, cv.COLOR_BGR2GRAY)
     edges = cv.Canny(gray, 50, 150, apertureSize=3)
     contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    return edges, contours
+    return contours, hierarchy
 
 #return the original image with the dices segmented and the list of dices.
-def segmentAndDrawDices(contours, image):
-    rects = []
+def segmentDices(contours):
+    dices = []
     for cnt in contours:
         rect = cv.minAreaRect(cnt)
         #filter out rects that don't have enough area to be dices
-        if rect[1][0]*rect[1][1] < 500 or rect[1][0]*rect[1][1] > 2000:
+        area = rect[1][0]*rect[1][1]
+        if area < 500 or area > 2000:
             continue
-        rects.append(rect)
-        box = cv.boxPoints(rect)
-        box = np.int0(box)
-        cv.drawContours(image, [box], 0, (0, 0, 255), 2)
-    return image, rects
+        dices.append(rect)
+
+    return dices
+
+#return the image of a die, given the contour, including its rotation.
+def extractDieImage(rect, image):
+    rows, cols = image.shape[:2]
+    rotation = cv.getRotationMatrix2D(rect[0], rect[2], 1)
+    rotated = cv.warpAffine(image, rotation, (cols, rows))
+    w, h = rect[1]
+    cropped = cv.getRectSubPix(rotated, (int(w), int(h)), rect[0])
+    return cropped
+
+#recents the image of a die, returning its contours and hierarchy.
+def getContoursDie(dieImage):
+    gray = cv.cvtColor(dieImage, cv.COLOR_BGR2GRAY)
+    rect, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    return contours, hierarchy
+
+def getDieNumber(contours, hierarchy):
+    # for each contour, try to fit a rect
+    dotRects = []
+    for cnt in contours:
+        # get the min area rect
+        rect = cv.minAreaRect(cnt)
+        # filter out rects that don't have enough area to be dices
+        area = rect[1][0]*rect[1][1]
+        if area < 8 or area > 150:
+            continue
+        dotRects.append(rect)
+    return dotRects
+
+
 
 #for 2 random images in dataset, segment the dices and draw them.
 for i in range(0, 2):
     file = random.choice(os.listdir(path))
     img = cv.imread(path + file)
-    result, contours = getCountours(img)
-    segmentedImage, rects = segmentAndDrawDices(contours, img.copy())
+    contours, hierarchy = getCountours(img)
+    dices = segmentDices(contours)
+    diceImages = []
+    for die in dices:
+        diceImages.append(extractDieImage(die, img))
+        dieImg = extractDieImage(die, img)
+
     #Show the Original image
     cv.imshow("Original "+file, img)
     cv.moveWindow("Original "+file, 1, img.shape[0]*i+1)
     #show the product of the contours extraction
-    cv.imshow(file, result)
-    cv.moveWindow(file, img.shape[1], img.shape[0]*i+1)
-    #Show the segmented dice
-    cv.imshow("contours "+file, segmentedImage)
-    cv.moveWindow("contours "+file, img.shape[1]*2+1, img.shape[0]*i+1)
-    print("number of dices in "+file + " is " + str(len(rects)))
+    #Show the segmented image
+    cv.imshow("Segmented "+file, cv.drawContours(img, contours, -1, (0, 255, 0), 3))
+    cv.moveWindow("Segmented "+file, img.shape[1], img.shape[0]*i+1)
+    print("number of dices in "+file + " is " + str(len(dices)))
+    #show dices
+    for j in range(0, len(diceImages)):
+        cv.imshow("Dice "+str(j)+" "+file, diceImages[j])
+        dieContours, dieHierarchy = getContoursDie(diceImages[j])
+        dieNumber = getDieNumber(dieContours, dieHierarchy)
+        print("Dice "+str(j)+" has the number "+str(len(dieNumber)))
+        cv.imshow("DiceContours "+str(j)+" "+file, cv.drawContours(diceImages[j], dieContours, -1, (0, 255, 0), 3))
+        #write dice number
+        cv.moveWindow("Dice "+str(j)+" "+file, img.shape[1]*2+1+(33*j), img.shape[0]*i+1)
+        cv.moveWindow("DiceContours "+str(j)+" "+file, img.shape[1]*2+1+(33*j), img.shape[0]*i+64)
+
+
     
 cv.waitKey(0)
